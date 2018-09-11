9a60186d870f1445ef19eb15b6422431083e6292
Torben Grove <torben_grove@live.dk>
2018-09-11 11:21:18 +0200

    Dagligfast done

--- MedicinOrdination/src/model/DagligFast.java
+++ MedicinOrdination/src/model/DagligFast.java
@@ -1,5 +1,34 @@
 package model;
 
-public class DagligFast {
-	// TODO
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class DagligFast extends Ordination {
+	Dosis[] dosiser;
+
+	public DagligFast(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel) {
+		super(startDen, slutDen, laegemiddel);
+		dosiser = new Dosis[4];
+	}
+
+	@Override
+	public double samletDosis() {
+		return doegnDosis() * ChronoUnit.DAYS.between(getStartDen(), getSlutDen());
+	}
+
+	@Override
+	public double doegnDosis() {
+		double antal = 0;
+		for (int i = 0; i < dosiser.length; i++) {
+			if (dosiser[i] != null) {
+				antal += dosiser[i].getAntal();
+			}
+		}
+		return antal;
+	}
+
+	@Override
+	public String getType() {
+		return getLaegemiddel().getEnhed();
+	}
 }
