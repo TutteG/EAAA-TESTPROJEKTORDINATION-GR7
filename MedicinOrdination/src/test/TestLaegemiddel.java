108e6aa6cff51f3ce93fd77e7431554e5277eac8
Torben Grove <torben_grove@live.dk>
2018-09-12 13:32:06 +0200

    Test klasser oprettet

---
+++ MedicinOrdination/src/test/TestLaegemiddel.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestLaegemiddel {
+	@Before
+	public void setUp() {
+
+	}
+
+	@Test
+	public void testSomething() {
+
+	}
+}
---
+++ MedicinOrdination/src/test/TestPatient.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestPatient {
+	@Before
+	public void setUp() {
+
+	}
+
+	@Test
+	public void testSomething() {
+
+	}
+}
